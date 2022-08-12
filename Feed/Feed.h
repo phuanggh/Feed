3eb7b51242e6b3d8ee5f71aa510576faca39963e
Penny Huang <pennyhuang@synology.com>
2022-08-12 14:50:12 +0800

    Create framework and FeedItem

---
+++ Feed/Feed.h
@@ -0,0 +1,18 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by Penny Huang on 2022/8/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Penny Huang on 2022/8/12.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
