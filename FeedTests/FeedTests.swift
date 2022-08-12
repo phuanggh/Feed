14940aa24745c1bab457900901b2784728ce88c6
Penny Huang <pennyhuang@synology.com>
2022-08-12 16:27:38 +0800

    move test logic to a separate type

--- FeedTests/FeedTests.swift
+++ FeedTests/FeedTests.swift
@@ -10,20 +10,31 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "a-requested-url")
+        HTTPClient.shared.get(from: URL(string: "a-requested-url")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {
+        
+    }
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class FeedTests: XCTestCase {
     
     func test_init_doseNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         // when feedLoader call load(), it requests something from the server
         // when feedLoader call load(), it invokes connection in its client
@@ -33,8 +44,8 @@ class FeedTests: XCTestCase {
     }
     
     func test_load_requestDataFromURL() {
-        
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         
         sut.load()
