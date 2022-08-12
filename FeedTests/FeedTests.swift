c7d4b46d79d3912b8a6f219f00c0594e8075ca0e
Penny Huang <pennyhuang@synology.com>
2022-08-12 16:15:22 +0800

    Request data from URL on

--- FeedTests/FeedTests.swift
+++ FeedTests/FeedTests.swift
@@ -9,23 +9,42 @@ import XCTest
 @testable import Feed
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "a-requested-url")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 class FeedTests: XCTestCase {
     
     func test_init_doseNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         // when feedLoader call load(), it requests something from the server
         // when feedLoader call load(), it invokes connection in its client
         // connection is made for a specific URL
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        // when it loads, we have to have request a URL through the client
+        // how can RemoteFeedLoader invokes a mothod in the client? -> dependency injection, inject a client to RemoteFeedLoader
+        // constructor injection
+        // property injection
+        // method injection
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
     override func setUpWithError() throws {
