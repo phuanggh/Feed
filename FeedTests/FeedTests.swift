5cea1f88aa376fad4d00ff64c7ade7c8a97b5f4d
Penny Huang <pennyhuang@synology.com>
2022-09-06 17:42:13 +0800

    delivers invalidData error on 200 response with invalid json

--- FeedTests/FeedTests.swift
+++ FeedTests/FeedTests.swift
@@ -82,6 +82,20 @@ class FeedTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidData() {
+        let (sut, client) = makeSUT()
+        
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load {
+            capturedError.append($0)
+        }
+        
+        let invalidJSON = Data("invalid JSON".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "test.injected.url")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -109,10 +123,10 @@ class FeedTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
     
