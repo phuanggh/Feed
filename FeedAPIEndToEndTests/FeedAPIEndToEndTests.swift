58f82f55ff38a37d8d31bc45372e43758ea0d0de
Penny Huang <pennyhuang@synology.com>
2022-11-17 15:06:47 +0800

    extract test setup to a hellper method

--- FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -11,21 +11,9 @@ import Feed
 final class FeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEdnTestServerGETFeedResult_matchesFixedTestAccountDate() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
-        let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 50.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items):
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             /// It's hard to debug if more than 1 fail
@@ -49,6 +37,23 @@ final class FeedAPIEndToEndTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    
+    private func getFeedResult() ->  LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 50.0)
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         FeedItem(id: id(at: index), description: description(at: index), location: location(at: index), imageURL: imageURL(at: index))
