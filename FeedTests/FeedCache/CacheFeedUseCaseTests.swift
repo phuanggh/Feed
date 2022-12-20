64fb05fe560cb237cec49312be790e2cec065ced
Penny Huang <pennyhuang@synology.com>
2022-12-20 15:06:54 +0800

    invert if logic to make code paths easier to follow

--- FeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ FeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -23,13 +23,14 @@ class LocalFeedLoader {
         // It's important to check those methods are invoked in the right order
         store.deleteCacheFeed { [weak self] error in
             guard let self = self else { return }
-            if error == nil {
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
                 self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
