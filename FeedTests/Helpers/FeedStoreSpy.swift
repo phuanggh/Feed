ef31ed43a7481a7c0a347ee7c9c30068c86f8711
Penny Huang <pennyhuang@synology.com>
2022-12-22 15:07:26 +0800

    load command requests cache retrieval

--- FeedTests/Helpers/FeedStoreSpy.swift
+++ FeedTests/Helpers/FeedStoreSpy.swift
@@ -15,6 +15,7 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessages:Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessages]()
@@ -45,5 +46,9 @@ class FeedStoreSpy: FeedStore {
     
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletion[index](nil)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
 }
